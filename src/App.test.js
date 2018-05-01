e00927631d203242e3dd87384f0a608aedb8191f
Liran Mery <liranm@ironsrc.com>
2018-05-01 07:55:41 +0300

    Writing a Counter Reducer with Tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+it('counter increments and decrements', () => {
+  expect(
+    counter(0, {type: 'INCREMENT'})
+  ).toEqual(1);
+
+  expect(
+    counter(1, {type: 'INCREMENT'})
+  ).toEqual(2);
+
+  expect(
+    counter(2, {type: 'DECREMENT'})
+  ).toEqual(1);
+
+  expect(
+    counter(1, {type: 'DECREMENT'})
+  ).toEqual(0);
+
+  expect(
+    counter(1, {type: 'SOMETHING_ELSE'})
+  ).toEqual(1);
+
+  expect(
+    counter(undefined, {})
+  ).toEqual(0);
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
