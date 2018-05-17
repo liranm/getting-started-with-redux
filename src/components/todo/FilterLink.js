2f241b55a7d4c6b94f2387547bc469e47ac94d3f
Liran Mery <liranm@ironsrc.com>
2018-05-17 08:58:32 +0300

    Generating Containers with connect() from React Redux (FooterLink)

--- src/components/todo/FilterLink.js
+++ src/components/todo/FilterLink.js
@@ -1,35 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from './Link';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export class FilterLink extends Component {
-    componentDidMount() {
-        const { store } = this.context;
-        this.unsubscribe = store.subscribe(() => {
-            this.forceUpdate();
-        });
-    }
-
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-    
-    render() {
-        const { filter, children } = this.props;
-        const { store } = this.context;
-        const state = store.getState();
-    
-        return (
-            <Link 
-                active={state.visibilityFilter === filter}
-                onClick={() => store.dispatch({
-                    type: 'SET_VISIBILITY_FILTER',
-                    filter
-                })} >{children}</Link>
-        );
-    }
-}
-
-FilterLink.contextTypes = {
-    store: PropTypes.object
+const mapStateToProps = (state, ownProps) => {
+    return {
+        active: state.visibilityFilter === ownProps.filter
+    };
 };
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        onClick: () => dispatch({
+            type: 'SET_VISIBILITY_FILTER',
+            filter: ownProps.filter
+        })
+    }; 
+};
+
+export const FilterLink = connect(mapStateToProps, mapDispatchToProps)(Link);
