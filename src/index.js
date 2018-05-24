5acda7efc38d09750ee6203495d912b9d5c8de53
Liran Mery <liranm@ironsrc.com>
2018-05-25 00:07:00 +0300

    Supplying the Initial State

--- src/index.js
+++ src/index.js
@@ -7,8 +7,16 @@ import { createStore } from 'redux';
 import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 
+const persistedState = {
+    todos: [{
+        id: 0,
+        text: 'Welcome Back',
+        completed: false
+    }]
+};
+
 ReactDOM.render(
-    <Provider store={createStore(rootReducer)}>
+    <Provider store={createStore(rootReducer, persistedState)}>
         <App />
     </Provider>, document.getElementById('root'));
 
