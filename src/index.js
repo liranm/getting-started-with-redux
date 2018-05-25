feff4c9571f3e7e3c19c7e5f3997cd036f6f1317
Liran Mery <liranm@ironsrc.com>
2018-05-25 11:39:16 +0300

    Refactoring the Entry Point

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import Root from './components/Root';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
-import rootReducer from './reducers';
-import { Provider } from 'react-redux';
-import { loadState, saveState } from './localStorage';
-import throttle from 'lodash/throttle';
+import configureStore from './configureStore'
 
-const persistedState = loadState();
-const store = createStore(rootReducer, persistedState);
-
-store.subscribe(throttle(() => {
-    saveState({
-        todos: store.getState().todos
-    });
-}, 1000));
+const store = configureStore();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root'));
+    <Root store={store} />, 
+    document.getElementById('root')
+);
 
 registerServiceWorker();
 
