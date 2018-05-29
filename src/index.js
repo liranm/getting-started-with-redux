d6460e62992233403e7067b0ee3379162a36e782
Liran Mery <liranm@ironsrc.com>
2018-05-30 00:05:04 +0300

    Adding a Fake Backend to the project

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Root from './components/Root';
 import registerServiceWorker from './registerServiceWorker';
-import configureStore from './configureStore'
+import configureStore from './configureStore';
+import { fetchTodos } from './api';
+
+fetchTodos('all').then(todos => 
+    console.log(todos));
 
 const store = configureStore();
 
