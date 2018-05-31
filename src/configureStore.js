30b213a66083d36b115e99a641b4a73d3a3a0ec5
Liran Mery <liranm@ironsrc.com>
2018-05-31 23:21:29 +0300

    The Middleware Chain

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,30 +1,24 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
-const addPromiseSupportToDispatch = (store) => {
-    const rawDispatch = store.dispatch;
+const promise = (store) => (next) => (action) => {
+    if(typeof action.then === 'function') {
+        return action.then(next);
+    }
 
-    return (action) => {
-        if(typeof action.then === 'function') {
-            return action.then(rawDispatch);
-        }
+    return next(action);
+};
 
-        return rawDispatch(action);
-    };
-}; 
-
-const addLoggingToDispatch = (store) => {
-    const rawDispatch = store.dispatch;
-
+const logger = (store) => (next) => {
     if(!console.group) {
-        return rawDispatch;
+        return next;
     }
 
     return (action) => {
         console.group(action.type);
         console.log('%c prev state', 'color:grey', store.getState());
         console.log('%c action', 'color:blue', action);
-        const returnValue = rawDispatch(action);
+        const returnValue = next(action);
         console.log('%c next state','color:green', store.getState());
         console.groupEnd(action.type);
 
@@ -32,14 +26,22 @@ const addLoggingToDispatch = (store) => {
     };
 };
 
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+    middlewares.slice().reverse().forEach(middleware => 
+        store.dispatch = middleware(store)(store.dispatch)
+    );
+};
+
 const configureStore = () => {
     const store = createStore(rootReducer);
+    const middlewares = [ promise ];
 
     if(process.env.NODE_ENV !== 'production') {
-        store.dispatch = addLoggingToDispatch(store);
+        middlewares.push(logger);
     }
 
-    store.dispatch = addPromiseSupportToDispatch(store);
+    wrapDispatchWithMiddlewares(store, middlewares);
     
     return store;
 };
