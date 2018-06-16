7cfd050063bfca31460ffbbf07f9a3f9502db9be
Liran Mery <liranm@ironsrc.com>
2018-06-16 23:55:27 +0300

    Inform the User of Interactions with CSS Transitions

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import Todo from './Todo';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const TodoList = ({
     todos,
     onTodoClick
 }) => (
-    <ul className="todo-list">
+    <TransitionGroup 
+        component="ul"
+        className="todo-list"
+    >
         {todos.map(todo => (
-            <Todo key={todo.id}
+            <CSSTransition 
+                timeout={{
+                    enter: 70,
+                    exit: 70
+                }}
+                classNames="todo-transition"
+            >
+                <Todo key={todo.id}
                 onClick={() => onTodoClick(todo.id)}
                 {...todo} />     
+            </CSSTransition>
         ))}
-    </ul>
+    </TransitionGroup>
 );
 
 export default TodoList;
